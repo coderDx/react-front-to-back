c614c6d253837492233969a89be7d3d689cc2c48
David Brixius <dbrixius@arccorp.com>
2023-06-17 19:59:49 -0500

    Video 33

--- feedback-app/src/context/FeedbackContext.js
+++ feedback-app/src/context/FeedbackContext.js
@@ -22,6 +22,11 @@ export const FeedbackProvider = ({ children }) => {
     },
   ])
 
+  const [feedbackEdit, setFeedbackEdit] = useState({
+    item: {},
+    edit: false
+  })
+
   const addFeedback = (newFeedback) => {
     newFeedback.id = uuidv4()
     setFeedback([newFeedback, ...feedback])
@@ -33,10 +38,18 @@ export const FeedbackProvider = ({ children }) => {
     }
   }
 
+  const editFeedback = (item) => {
+    setFeedbackEdit({
+      item,
+      edit: true
+    })
+  }
+
   return <FeedbackContext.Provider value={{
     feedback,
     deleteFeedback,
-    addFeedback
+    addFeedback,
+    editFeedback
   }}>
     {children}
   </FeedbackContext.Provider>
