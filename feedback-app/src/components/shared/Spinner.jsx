593ac8cabc374149c181cf783d919ae1aa1d823f
David Brixius <dbrixius@arccorp.com>
2023-06-18 13:09:44 -0500

    Video 41

---
+++ feedback-app/src/components/shared/Spinner.jsx
@@ -0,0 +1,19 @@
+import spinner from '../assets/spinner.gif'
+
+function Spinner() {
+  return (
+    <div>
+      <img 
+        src={spinner}
+        alt='Loading...'
+        style={{
+          width:'100px',
+          margin:'auto',
+          display:'block'
+        }}
+      />
+    </div>
+  )
+}
+
+export default Spinner
