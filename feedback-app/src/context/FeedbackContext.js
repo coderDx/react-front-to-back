f1e666a8f5b600bbb53f9d9e5e35ee857144ab5a
David Brixius <dbrixius@arccorp.com>
2023-06-18 14:16:55 -0500

    Video 42

--- feedback-app/src/context/FeedbackContext.js
+++ feedback-app/src/context/FeedbackContext.js
@@ -1,5 +1,4 @@
 import { createContext, useState, useEffect } from "react"
-import { v4 as uuidv4 } from 'uuid'
 
 const FeedbackContext = createContext()
 
@@ -24,9 +23,18 @@ export const FeedbackProvider = ({ children }) => {
     setIsLoading(false)
   }
 
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4()
-    setFeedback([newFeedback, ...feedback])
+  const addFeedback = async (newFeedback) => {
+    const response = await fetch("http://localhost:5000/feedback", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newFeedback)
+    })
+
+    const data = await response.json()
+
+    setFeedback([data, ...feedback])
   }
 
   const deleteFeedback = (id) => {
