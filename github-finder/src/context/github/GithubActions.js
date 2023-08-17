ad77413997d0dc26cabf5acfbc2c51d102ac5c89
Dave Brixius <dbrixius@arccorp.com>
2023-08-16 19:41:37 -0500

    Video 68

--- github-finder/src/context/github/GithubActions.js
+++ github-finder/src/context/github/GithubActions.js
@@ -1,62 +1,31 @@
+import axios from 'axios'
+
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN
+
+const github = axios.create({
+  baseURL: GITHUB_URL,
+  headers: { 'Authorization': GITHUB_TOKEN }
+})
 
 export const searchUsers = async (text) => {
   const params = new URLSearchParams({
     q: text
   })
 
-  const response = await fetch(
-    `${GITHUB_URL}/search/users?${params}`,
-    {
-      headers: {
-        Authorization: `${GITHUB_TOKEN}`
-      }
-    }
-  )
+  const response = await github.get(`/search/users?${params}`)
 
-  const { items } = await response.json()
-
-  return items
+  return response.data.items
 }
 
-export const getUser = async (login) => {
+export const getUserAndRepos = (async(login)=>{
+  const [user, repos] = await Promise.all([
+    github.get(`users/${login}`),
+    github.get(`users/${login}/repos`)
+  ])
 
-  const response = await fetch(
-    `${GITHUB_URL}/users/${login}`,
-    {
-      headers: {
-        Authorization: `${GITHUB_TOKEN}`
-      }
-    }
-  )
-
-  if(response.status === 404) {
-    window.location = '/not-found'
-  } else {
-    const data = await response.json()
-
-    return data
+  return {
+    user: user.data, 
+    repos: repos.data
   }
-}
-
-export const getUserRepos = async (login) => {
-
-  const params = new URLSearchParams({
-    sort: 'created',
-    per_page: 10
-  })
-
-  const response = await fetch(
-    `${GITHUB_URL}/users/${login}/repos?${params}`,
-    {
-      headers: {
-        Authorization: `${GITHUB_TOKEN}`
-      }
-    }
-  )
-
-  const data = await response.json()
-
-  return data
-}
+})
